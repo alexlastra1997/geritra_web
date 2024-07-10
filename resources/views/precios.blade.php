44204b7080fba24d6cc05dc64cccf4dba48aa054
Franz Guzmán <fguzman@stackbuilders.com>
2024-07-10 14:58:25 -0500

    fix route

--- resources/views/precios.blade.php
+++ resources/views/precios.blade.php
@@ -6,7 +6,7 @@
       <div class="mx-auto max-w-screen-md text-center mb-8 lg:mb-12">
         <h2 class="m-7 text-4xl md:text-5xl tracking-tight font-extrabold text-blue-950 dark:text-white">PLANES EMPRESARIALES</h2>
           <p class="mb-5 font-light text-gray-500 sm:text-xl dark:text-gray-400">Optimiza la seguridad y bienestar en tu empresa con PROTEGE SST. Nuestro sistema avanzado te permite gestionar eficientemente los riesgos laborales y garantizar un ambiente de trabajo seguro y saludable. Elige el plan que mejor se adapte a tus necesidades y comienza a proteger lo más valioso de tu organización: tu equipo. ¡Únete a PROTEGE SST hoy y da el primer paso hacia un entorno laboral más seguro!</p>
-          <a href="{{ route('contactos') }}" class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-lime-500 hover:bg-lime-700 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
+          <a href="{{ route('contactos.index') }}" class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-lime-500 hover:bg-lime-700 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
             Contáctanos
             <svg class="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
